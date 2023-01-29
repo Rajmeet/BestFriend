f22847050f48013250f6f8f36fb90c51f5e2c4ce
Kollin <Bartzkol@msu.edu>
2023-01-29 03:21:09 -0500

    fixed name of app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Best Friend',
+      title: 'Bestfriend',
       home: HomeScreen(),
     );
   }
