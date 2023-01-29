27a1eeb6cafb5a64918c88d61afd72c408454503
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2023-01-29 11:03:13 -0500

    Added NLP Bot to detect chat

--- lib/gcp_service.dart
+++ lib/gcp_service.dart
@@ -1,9 +1,15 @@
 import 'package:http/http.dart' as http;
 
 Future<List> fetchData(String inputText, String mainPrompt) async {
-  var url = Uri.https('helloworld-25fw3da4la-uc.a.run.app', 'help', {
-    'input': inputText,
-    'main_prompt': mainPrompt
+  var url = Uri.https('helloworld-25fw3da4la-uc.a.run.app', 'help',
+      {'input': inputText, 'main_prompt': mainPrompt});
+  var response = await http.get(url);
+  return [response.statusCode, response.body];
+}
+
+Future<List> fetchCheckThots(String inputText) async {
+  var url = Uri.https('helloworld-25fw3da4la-uc.a.run.app', 'check', {
+    'checkmsg': inputText,
   });
   var response = await http.get(url);
   return [response.statusCode, response.body];
