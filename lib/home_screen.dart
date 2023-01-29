a0092046f1a6a9c4826c978b7ef584e0b76e304f
Kollin <Bartzkol@msu.edu>
2023-01-29 01:11:19 -0500

    We got the tree homepage to work

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,7 +17,45 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 172, 98, 209),
-        title: Text("Home Screen"),
+        title: const Text("Home Screen"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Container(
+              height: 1000,
+              width: 1000,
+              decoration: const BoxDecoration(
+                gradient: RadialGradient(
+                  colors: [
+                    Color(0xffA579A6),
+                    Color(0xff675277),
+                    Color(0xff29243B),
+                    Color(0xff220B33),
+                  ],
+                  radius: 1.3,
+                  focal: Alignment(.25, .45),
+                ),
+                image: DecorationImage(
+                  opacity: .7,
+                  image: AssetImage("assets/happyTree.png"),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 5,
+            left: 5,
+            right: 5,
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                  "Welcome to Bestfriend!\nYou are never too old\nto meet your new\nBestfriend.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 32)),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: CustomNavBar(3),
     );
---
+++ lib/resources.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'about.dart';
+import 'tos.dart';
+import 'custom_navbar.dart';
+
+class ResourceScreen extends StatefulWidget {
+  const ResourceScreen({Key? key}) : super(key: key);
+
+  @override
+  _ResourceScreenState createState() => _ResourceScreenState();
+}
+
+class _ResourceScreenState extends State<ResourceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.home),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        backgroundColor: const Color.fromARGB(255, 172, 98, 209),
+        title: const Text("Theropists"),
+        actions: <Widget>[
+          PopupMenuButton<int>(
+            onSelected: (value) {
+              if (value == 1) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToS()),
+                );
+              } else if (value == 2) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const About()),
+                );
+              }
+            },
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem(
+                  value: 1,
+                  child: Text("Terms of Service"),
+                ),
+                const PopupMenuItem(
+                  value: 2,
+                  child: Text("About"),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: CustomNavBar(0),
+    );
+  }
+}
