2c6bb8ae51f9f27e3b7d248f2844d430702364e4
davidrackerby <rackerb1@msu.edu>
2023-01-29 02:29:57 -0500

    Add assets to make the text bubbles

---
+++ lib/hotline_assets/message_received_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:math' as math; // import this
+
+import 'package:flutter/material.dart';
+import 'custom_shape.dart';
+
+class ReceivedMessageScreen extends StatelessWidget {
+  final String message;
+  const ReceivedMessageScreen({
+    required Key key,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final messageTextGroup = Flexible(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.rotationY(math.pi),
+              child: CustomPaint(
+                painter: CustomShape(Colors.grey.shade300),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                padding: EdgeInsets.all(14),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(18),
+                    bottomLeft: Radius.circular(18),
+                    bottomRight: Radius.circular(18),
+                  ),
+                ),
+                child: Text(
+                  message,
+                  style: TextStyle(color: Colors.black, fontSize: 14),
+                ),
+              ),
+            ),
+          ],
+        ));
+
+    return Padding(
+      padding: EdgeInsets.only(right: 50.0, left: 18, top: 10, bottom: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          SizedBox(height: 30),
+          messageTextGroup,
+        ],
+      ),
+    );
+  }
+}
